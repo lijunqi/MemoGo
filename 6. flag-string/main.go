3fb05c52a0ded8b600e98348a70ed36f2b7d9b5a
Jacky Junqi Li <jli21@rockwellautomation.com>
2024-11-14 14:31:30 +0800

    flag

--- 6. flag-string/main.go
+++ 6. flag-string/main.go
@@ -13,7 +13,13 @@ func main() {
 	path := flag.String("path", "myapp.log", "description 1")
 	level := flag.String("level", "ERROR", "description 2")
 
+	var flagVar int
+	flag.IntVar(&flagVar, "flagname", 1234, "help message for flagname")
+	fmt.Printf("1. flagVar: %d\n", flagVar)
+
 	flag.Parse()
+
+	fmt.Printf("2. flagVar: %d\n", flagVar)
 
 	f, err := os.Open(*path)
 	if err != nil {
